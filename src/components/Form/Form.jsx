551fa18d99b6c32c2ec28569f4f966a8e65ab070
Rayan0707 <rusl070769@gmail.com>
2023-10-10 22:52:32 +0400

    init

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import'./Form.css'
+const Form = () => {
+    return (
+        <div className={"form"}>
+            <h3>Enter your details</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Улица'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Физ. лицо</option>
+                <option value={'legal'}>Юр. лицо</option>
+            </select>
+        </div>
+    );
+};
+
+export default Form;
